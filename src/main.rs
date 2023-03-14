b3b1c75a6ec4c3722ec67b6200d02bf0ea03f906
Emilio Ziniades <emilioziniades@protonmail.com>
2023-03-14 20:33:45 +0200

    hello world rocket app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, launch, routes};
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
+}
+
+#[get("/<_..>")]
+fn hello() -> &'static str {
+    "Hello world!"
 }
